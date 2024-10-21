4970951276dc97db09733c08ced8d088b711e551
Sowjanyadusanapudi <sowjanyadusanapudi531@gmail.com>
2024-10-21 12:10:04 +0530

    Create vowelornot.c

---
+++ vowelornot.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    char c;
+    printf("enter an alphabet:");
+    scanf("%c",&c);
+    if(c=='a'|| c=='e'|| c=='i'|| c=='o'|| c=='u')
+{
+    printf("the charecter is an vowel ");
+}
+else
+{
+    printf("the charecter is not an vowel");
+}
+}
