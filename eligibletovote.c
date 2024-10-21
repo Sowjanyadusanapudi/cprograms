47f5a6cf932ba174bff677862d55f85ca890952d
Sowjanyadusanapudi <sowjanyadusanapudi531@gmail.com>
2024-10-21 11:47:33 +0530

    Create eligibletovote.c

---
+++ eligibletovote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    int a;
+    printf("enter a");
+    scanf("%d",&a);
+    if(a>=18)
+{
+    printf("the person is eligible to vote ");
+}
+else
+{
+    printf("the person is not eligible to vote");
+}
+}
