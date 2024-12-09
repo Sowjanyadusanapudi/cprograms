bd65f77d3ed1107e75b4f6f2e15ff0bb3e2a8e48
Sowjanyadusanapudi <sowjanyadusanapudi531@gmail.com>
2024-12-09 22:39:16 +0530

    Create perimeterofrec.c

---
+++ perimeterofrec.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main() 
+{
+int l,b;
+float perimeter;
+    printf("Enter the value of l,b:");
+scanf("%d%d",&l,&b);
+perimeter= 2*(l+b);
+printf("The perimeter is %f",perimeter);
+}
