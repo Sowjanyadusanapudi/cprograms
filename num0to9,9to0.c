bba516cee4cb19d1a5703af158e2092727695ebe
Sowjanyadusanapudi <sowjanyadusanapudi531@gmail.com>
2024-12-29 16:24:56 +0530

    Create num0to9,9to0.c

---
+++ num0to9,9to0.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+    int i;
+  for(i=0;i<=10;++i)
+  {
+      printf("%d\t",i);
+  }
+  printf("\n");
+  for(i=10;i>=0;--i)
+  {
+      printf("%d\t",i);
+  }
+}
